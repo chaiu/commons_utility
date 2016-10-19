3cb489fa10eb07c4d03de95a35326f36aa21dfde
hellozjf <908686171@qq.com>
2016-10-19 21:55:28 +0800

    提交作业，但只是写完java代码，测试部分还没写

---
+++ src/main/java/com/topcoder/util/log/Level.java
@@ -0,0 +1,17 @@
+package com.topcoder.util.log;
+
+/**
+ */
+public class Level {
+	
+	public static final Level DEBUG = new Level(1);
+
+	public static final Level ERROR = new Level(2);
+	
+	private int level;
+	
+	private Level(int level) {
+		this.level = level;
+	}
+}
+
---
+++ src/main/java/com/topcoder/util/log/Log.java
@@ -0,0 +1,9 @@
+package com.topcoder.util.log;
+
+/**
+ */
+public interface Log {
+
+	void log(Level level, String message);
+}
+
