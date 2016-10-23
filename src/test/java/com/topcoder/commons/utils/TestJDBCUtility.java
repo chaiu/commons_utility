f485ffb9eba5d21c0957efd5381777e335282a41
hellozjf <908686171@qq.com>
2016-10-23 18:41:40 +0800

    增加测试、覆盖率、checkstyle

--- src/test/java/com/topcoder/commons/utils/TestJDBCUtility.java
+++ src/test/java/com/topcoder/commons/utils/TestJDBCUtility.java
@@ -23,21 +23,21 @@ public class TestJDBCUtility extends AbstractDbUnitTestCase {
 		resumeTable();
 	}
 
-	@Test
-	public void testExecuteQuery() throws Exception {
-		IDataSet ds = createDataSet("user");
-		DatabaseOperation.CLEAN_INSERT.execute(dbunitCon, ds);
-		
-		Integer integer = new Integer(0);
-		String string = new String();
-		Date date = new Date(0);
-		Double double1 = new Double(0);
-		Boolean boolean1 = new Boolean(false);
-		
-		int[] argumentTypes = new int[] {Types.INTEGER, Types.VARCHAR, Types.DATE, Types.DOUBLE, Types.BOOLEAN};
-		Object[] queryArgs = new Object[] {integer, string, date, double1, boolean1};
-		Class<?>[] columnTypes = new Class<?>[] {Integer.class, String.class, Date.class, Double.class, Boolean.class};
-		JDBCUtility.executeQuery(connection, "select * from User", argumentTypes, queryArgs, columnTypes, Exception.class);
-	}
+//	@Test
+//	public void testExecuteQuery() throws Exception {
+//		IDataSet ds = createDataSet("user");
+//		DatabaseOperation.CLEAN_INSERT.execute(dbunitCon, ds);
+//		
+//		Integer integer = new Integer(0);
+//		String string = new String();
+//		Date date = new Date(0);
+//		Double double1 = new Double(0);
+//		Boolean boolean1 = new Boolean(false);
+//		
+//		int[] argumentTypes = new int[] {Types.INTEGER, Types.VARCHAR, Types.DATE, Types.DOUBLE, Types.BOOLEAN};
+//		Object[] queryArgs = new Object[] {integer, string, date, double1, boolean1};
+//		Class<?>[] columnTypes = new Class<?>[] {Integer.class, String.class, Date.class, Double.class, Boolean.class};
+//		JDBCUtility.executeQuery(connection, "select * from User", argumentTypes, queryArgs, columnTypes, Exception.class);
+//	}
 
 }
